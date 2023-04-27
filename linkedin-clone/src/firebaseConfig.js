c5ea5071ea5787363bc05daab0d33400850e3d3b
Mert Öden <93067736+mertoden@users.noreply.github.com>
2023-04-27 05:24:19 +0300

    Base Setup V1

---
+++ linkedin-clone/src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDOZ0mhvdMVuR_ucWODuIRqoocngE9dKCU",
+  authDomain: "linkedindb-5ea46.firebaseapp.com",
+  projectId: "linkedindb-5ea46",
+  storageBucket: "linkedindb-5ea46.appspot.com",
+  messagingSenderId: "543421588294",
+  appId: "1:543421588294:web:f15bed32dc9aa35fb97b47"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
